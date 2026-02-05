0f9eafaf7ffd720265821ddb01c7c4d9dbca1704
normanstrydom <normans@felixsol.co.za>
2026-02-05 16:17:59 +0200

    Output start

--- src/main/java/za/co/felixsol/wildfly/util/WfJgroupsEEView.java
+++ src/main/java/za/co/felixsol/wildfly/util/WfJgroupsEEView.java
@@ -7,18 +7,18 @@ import org.jboss.as.controller.client.ModelControllerClient;
 import org.jboss.dmr.ModelNode;
 
 public class WfJgroupsEEView {
-    
+
     public static void main(String[] args) throws Exception {
-	
-	String user = "****";
-	String password = "*************";
+
+	String user = "*****";
+	String password = "************";
 	int port = 9990;
 //	String host = "workstation43-wf-20";
 	String host = "workstation43-wf-39";
-	
+
 	try (ModelControllerClient wildflyClient = ModelControllerClientFactory.getClient(host, port, user,
-		    password)) {
-	    
+		password)) {
+
 	    DefaultOperationRequestBuilder builder = new DefaultOperationRequestBuilder();
 	    builder.addNode("host", "*");
 	    builder.addNode("server", "*");
@@ -26,12 +26,22 @@ public class WfJgroupsEEView {
 	    builder.addNode("channel", "ee");
 	    builder.setOperationName("query");
 	    ModelNode request = builder.buildRequest();
-            ModelNode outcome = wildflyClient.execute(request);
-            
-            System.out.println(outcome.toJSONString(false));
-            
+	    ModelNode outcome = wildflyClient.execute(request);
+
+	    System.out.println(outcome.toJSONString(false));
+
+	    StringBuilder sb = new StringBuilder();
+	    outcome.get("result").asList().forEach(node -> {
+		String hostName = node.get("address").asPropertyList().get(0).getValue().asString();
+		String serverName = node.get("address").asPropertyList().get(1).getValue().asString();
+		String view = node.get("result").get("view").asString();
+		sb.append(String.format("%s/%s\n", hostName, serverName));
+		sb.append(String.format("\t%s\n", view));
+	    });
+	    System.out.println(sb.toString());
+
 	}
-	
+
     }
 
 }
